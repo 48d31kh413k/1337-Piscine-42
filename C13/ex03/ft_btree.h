8f6ae1d5df5baa824c809119adcb79ffac94286d
aboutahri <abdelkhalekboutahri@gmail.com>
2021-09-16 20:01:46 +0100

    1337bg

---
+++ C13/ex03/ft_btree.h
@@ -0,0 +1,13 @@
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+
+typedef struct s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}		t_btree;
+
+t_btree	*btree_create_node(void *item);
+
+#endif
